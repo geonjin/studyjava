c109e7b46a591e4e84b76d6407a2faf31de8a1bb
DESKTOP-5STRNK1\ca990125a <ca990125a@naver.com>
2022-07-26 17:35:40 +0900

    [ADD]  계산기 지속하는 것과 종료하는 부분 코드 변경

--- calculator/src/controller/Userinput.java
+++ calculator/src/controller/Userinput.java
@@ -3,23 +3,14 @@ import java.util.Scanner;
 import model.Arithmetic;
 
 
-class Qmsg{
-	public void qmsg() {
-		
-	System.out.println("계산을 계속하시려면 1번, 계산기를 종료하시고 싶으시면 2번을 눌러 주세요");
-	
-	 
-	 }
-	}
-
-
-
 public class Userinput {
 	public void userinput() {
 	Scanner sc = new Scanner(System.in);
 	Arithmetic oper =  new Arithmetic();{
 		Qmsg msg = new Qmsg();
-		int choice = 0;
+	
+		
+	loop:
 	for(int i = 0; i<100; i++) { 
 
 		System.out.println("- - - - - - - - -");
@@ -51,23 +42,28 @@ public class Userinput {
 			System.out.printf("%d %s %d = %f 입니다\n",num1,operator,num2,result);
 			System.out.println("- - - - - - - - -");
 			}
-		msg.qmsg();
-		choice = sc.nextInt();
+		
+		for(int choice = 0; choice < 100; choice++) {
+	
+		System.out.println("계산을 계속하시려면 1번, 계산기를 종료하시고 싶으시면 2번을 눌러 주세요");
+			choice = sc.nextInt();
 			if(choice == 1)
-			continue;
+		    break ;
 			
 		    else if (choice == 2) {
-		
 			System.out.println("계산기를 종료합니다");
-			break;
+			break loop;
 		    }
-			else if(choice != 1 && choice != 2 ) {
+		    else if(choice != 1 && choice != 2 ) {
 				System.out.println("잘 못 입력하셨습니다. 다시 입력해주세요");
-				msg.qmsg();
+			 		}
+		
 			}
-	// 위에 if문을 switch 문으로 바꾸고 default: 연산자가 아닌거 나왔을때 예외처리 하기
-			}
+	
+		}	
 		}
 	}
 }
+
+
 
